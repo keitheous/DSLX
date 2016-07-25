7a3da173cffde41d69f65c4ebe0a8049bb98def2
keitheous <keithchongwy@gmail.com>
2016-07-25 16:55:50 +1000

    created user model

---
+++ db/migrate/20160725065121_create_users.rb
@@ -0,0 +1,18 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :firstname
+      t.string :surname
+      t.string :email
+      t.password_digest :password
+      t.string :bgcolor
+      t.string :textcolor
+      t.string :fontsize
+      t.string :wordspacing
+      t.string :paragraphspacing
+      t.boolean :highlights
+
+      t.timestamps null: false
+    end
+  end
+end
