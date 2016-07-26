508880f35de0eb3c905731f751d11613084bd9e7
Christopher Lee <hanernlee@gmail.com>
2016-07-26 10:53:44 +1000

    adding scraper

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  require 'open-uri'
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    @user = User.new
+    @user.firstname = params[:firstname]
+    @user.surname = params[:lastname]
+    @user.email = params[:email]
+    @user.password = params[:password]
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/users'
+    else
+      redirect_to '/'
+    end
+  end
+
+  def show
+  end
+
+  def url
+    @url = params[:url]
+    parse_page = Nokogiri::HTML(open(@url))
+
+    @content = parse_page.css('div').text
+    # @array_content = []
+    #
+    # parse_page.css('div').map do |content|
+    #   content_text = content.text
+    #   @array_content.push(content_text)
+    # end
+    #
+
+  end
+
+end
